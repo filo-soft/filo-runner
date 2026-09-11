95361299b2672bcaecc5c5b6e62ee261515f838e
filo-soft <miha294824@gmail.com>
2026-09-11 11:07:19 +0300

    Enable persistent coin balance and title fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./slide-legs-fix";
 import "./audio-fix";
 import "./game-features";
 import "./late-game-fix";
+import "./economy-fix";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
