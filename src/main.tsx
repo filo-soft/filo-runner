94188824c0b01007123c5f9e937f284cca84cc38
filo-soft <miha294824@gmail.com>
2026-09-15 17:24:36 +0300

    Load game over layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./mobile-hud.css";
+import "./game-over-layout.css";
 import "./ui-defaults";
 import "./difficulty";
 import "./temple-roof-fix";
