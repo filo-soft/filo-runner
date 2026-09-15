1d1369d99e3a28b4887a5321a23d27477f2fcfc1
filo-soft <miha294824@gmail.com>
2026-09-15 18:18:35 +0300

    feat: wire IVAN into runner

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import "./economy-fix";
 import "./cityscape-fix";
 import "./cityscape-sparse-fix";
 import "./branding-fix";
+import "./ivan";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
