a9c30a5ff20bbdc2fa1461d10cfb7bdd3e7d9871
filo-soft <miha294824@gmail.com>
2026-09-15 12:11:31 +0300

    Enable Ivan chaser system

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import "./audio-fix";
 import "./game-features";
 import "./late-game-fix";
 import "./economy-fix";
+import "./chaser-fix";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
