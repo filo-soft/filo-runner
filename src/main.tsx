757b67758cb0be5e21533b14bcc1336f1363b8ad
filo-soft <miha294824@gmail.com>
2026-09-16 16:01:09 +0300

    Enable temple tunnel gameplay section

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import "./cityscape-sparse-fix";
 import "./branding-fix";
 import "./hit-protection";
 import "./boulder-fix";
+import "./temple-tunnel-fix";
 import "./ivan-guard-fix";
 import "./version-fix";
 import "./ui-readability-fix";
