af4dcb199072c583b20b20b96aacdccc73880a2c
filo-soft <miha294824@gmail.com>
2026-09-16 08:38:19 +0300

    Remove Zeus integration

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,6 @@ import "./cityscape-fix";
 import "./cityscape-sparse-fix";
 import "./branding-fix";
 import "./hit-protection";
-import "./zeus-fix";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
